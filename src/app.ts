0c7ef04fab25b2ff1d09b762723adbc4b75cf37c
khairul-islam98 <janeyrahman900@gmail.com>
2024-12-01 22:50:15 +0600

    Implement Express server setup with routing and error handling

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import router from "./app/routes";
+import globalErrorHandler from "./app/middlewares/globalErrorHandler";
+import { notFound } from "./app/error/not-found";
+
+const app: Application = express();
+app.use(cookieParser());
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World");
+});
+
+app.use("/api", router);
+
+app.use(notFound);
+app.use(globalErrorHandler);
+
+export default app;
