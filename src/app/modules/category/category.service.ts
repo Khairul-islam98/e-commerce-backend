27e91654a8df64829690632f1977c5d4f7207b15
khairul-islam98 <janeyrahman900@gmail.com>
2024-12-03 17:47:04 +0600

    Add category service with methods for creating, retrieving, updating, and deleting categories

---
+++ src/app/modules/category/category.service.ts
@@ -0,0 +1,43 @@
+import { Category } from "@prisma/client";
+import prisma from "../../utils/prisma";
+import AppError from "../../error/AppError";
+import httpStatus from "http-status";
+
+const createCategoryIntoDB = async (payload: Category) => {
+  const isExist = await prisma.category.findFirst({
+    where: {
+      name: payload.name,
+    },
+  });
+  if (isExist) {
+    throw new AppError(httpStatus.BAD_REQUEST, "Category already exist");
+  }
+  return await prisma.category.create({
+    data: payload,
+  });
+};
+const getAllCategories = async () => {
+  return await prisma.category.findMany();
+};
+const updateCategory = async (id: string, payload: Category) => {
+  return await prisma.category.update({
+    where: {
+      id,
+    },
+    data: payload,
+  });
+};
+const deleteCategory = async (id: string) => {
+  return await prisma.category.delete({
+    where: {
+      id,
+    },
+  });
+};
+
+export const CategoryService = {
+  createCategoryIntoDB,
+  getAllCategories,
+  updateCategory,
+  deleteCategory,
+};
