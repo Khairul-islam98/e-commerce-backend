367d1c729c25d782d1a233f19b43cf317be7f6bc
khairul-islam98 <janeyrahman900@gmail.com>
2024-12-03 17:47:24 +0600

    Add category routes for creating, retrieving, updating, and deleting categories

---
+++ src/app/modules/category/category.route.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { CategoryController } from "./category.controller";
+
+const router = express.Router();
+
+router.post("/", CategoryController.createCategory);
+router.get("/", CategoryController.getAllCategories);
+router.patch("/:categoryId", CategoryController.updateCategory);
+router.delete("/:categoryId", CategoryController.deleteCategory);
+
+export const CategoryRoutes = router;
