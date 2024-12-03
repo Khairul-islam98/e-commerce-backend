692d4c5f6302c319bbb368490e505eeee9c9ed54
khairul-islam98 <janeyrahman900@gmail.com>
2024-12-03 11:52:47 +0600

    Add product routes for CRUD operations and category-related product retrieval

---
+++ src/app/modules/product/product.route.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import { ProductController } from "./product.controller";
+import auth from "../../middlewares/auth";
+import { UserRole } from "@prisma/client";
+import { fileUpload } from "../../utils/fileUpload";
+
+const router = express.Router();
+
+router.post(
+  "/create",
+  auth(UserRole.VENDOR, UserRole.ADMIN),
+  fileUpload.single("image"),
+  ProductController.createProduct
+);
+router.post(
+  "/duplicate/:productId",
+  auth(UserRole.VENDOR, UserRole.ADMIN),
+  fileUpload.single("image"),
+  ProductController.duplicateProduct
+);
+router.put(
+  "/update/:productId",
+  auth(UserRole.VENDOR, UserRole.ADMIN),
+  fileUpload.single("image"),
+  ProductController.updateProduct
+);
+router.delete(
+  "/delete/:productId",
+  auth(UserRole.VENDOR, UserRole.ADMIN),
+  ProductController.deleteProduct
+);
+router.get("/", ProductController.getAllProducts);
+router.get("/:productId", ProductController.getProductById);
+router.get(
+  "/category-related/:categoryId",
+  ProductController.getProductsByCategory
+);
+router.get(
+  "/shop-follower",
+  auth(UserRole.CUSTOMER),
+  ProductController.getShopFollowerProducts
+);
+
+export const ProductRoutes = router;
