4ee27807c912d1ea96db7152b113933f13d7800f
khairul-islam98 <janeyrahman900@gmail.com>
2024-12-03 17:47:15 +0600

    Add category controller with methods for creating, retrieving, updating, and deleting categories

---
+++ src/app/modules/category/category.controller.ts
@@ -0,0 +1,46 @@
+import catchAsync from "../../utils/catchAsync";
+import sendResponse from "../../utils/sendResponse";
+import { CategoryService } from "./category.service";
+import httpStatus from "http-status";
+
+const createCategory = catchAsync(async (req, res) => {
+  const result = await CategoryService.createCategoryIntoDB(req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.CREATED,
+    success: true,
+    message: "Category created successfully",
+    data: result,
+  });
+});
+const getAllCategories = catchAsync(async (req, res) => {
+  const result = await CategoryService.getAllCategories();
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    data: result,
+  });
+});
+const updateCategory = catchAsync(async (req, res) => {
+  const result = await CategoryService.updateCategory(req.params.id, req.body);
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: "Category updated successfully",
+    data: result,
+  });
+});
+const deleteCategory = catchAsync(async (req, res) => {
+  await CategoryService.deleteCategory(req.params.id);
+  sendResponse(res, {
+    statusCode: httpStatus.NO_CONTENT,
+    success: true,
+    message: "Category deleted successfully",
+  });
+});
+
+export const CategoryController = {
+  createCategory,
+  getAllCategories,
+  updateCategory,
+  deleteCategory,
+};
