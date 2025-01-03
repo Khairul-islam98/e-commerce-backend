18fc367b24c29b4cd22b3cf17b85d2c6d8dffe9b
khairul-islam98 <janeyrahman900@gmail.com>
2025-01-03 22:25:03 +0600

    Add utility function to standardize API responses

---
+++ src/app/utils/sendResponse.ts
@@ -0,0 +1,25 @@
+import { response, Response } from "express";
+
+type ResponseType<T> = {
+  statusCode: number;
+  success: boolean;
+  message?: string;
+  meta?: {
+    page: number;
+    limit: number;
+    total: number;
+  };
+  data?: T;
+};
+
+const sendResponse = <T>(res: Response, data: ResponseType<T>) => {
+  res.status(data.statusCode).json({
+    success: data.success,
+    status: data.statusCode,
+    message: data.message,
+    meta: data.meta,
+    data: data.data,
+  });
+};
+
+export default sendResponse;
