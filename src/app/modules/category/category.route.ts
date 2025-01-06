734e18b5c76c6b3ad416c9e0933f05c4937e27bc
khairul-islam98 <janeyrahman900@gmail.com>
2025-01-06 17:49:07 +0600

    Add category routes for CRUD operations in category management

---
+++ src/app/modules/category/category.route.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { CategoryController } from "./category.controller";
+
+const router = express.Router();
+
+router.post("/", CategoryController.createCategory);
+router.get("/", CategoryController.getAllCategories);
+router.get("/:categoryId", CategoryController.getCategoryById);
+router.patch("/:categoryId", CategoryController.updateCategory);
+router.delete("/:categoryId", CategoryController.deleteCategory);
+
+export const CategoryRoutes = router;
